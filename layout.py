34d3e9c36e598fd3091a8a0758a930ffc28fc5a2
hovhannisyan91 <khovhannisyan91@yahoo.com>
2022-11-16 22:20:04 +0400

    minor updates

--- layout.py
+++ layout.py
@@ -2,60 +2,81 @@ from dash import html, dcc, Dash, dash_table
 from dash.dependencies import Input, Output, State
 from datetime import datetime as dt, timedelta
 import plotly.express as px
-
-from components.figures import df
+import pandas_datareader.data as web
+# from components.data import options
 import pandas as pd
 
+		
 app=Dash(__name__)
-app.title = 'Sample App'
-    
+app.title = 'Stock Ticker Dashboard'
+	
 app.layout = html.Div([
-    
-    html.Div([
+	html.H1('Stock Ticker Dashboard'),
+	html.Hr(),
+	# This div contains input forms and submit button
+	html.Div([
+		# Block 1
+		html.Div([
+			html.Label('Select Stock Symbol:'),
+			dcc.Dropdown(id='stock-picker',
+						options=[
+							{'label':'Amazon','value':'AMZN'},
+							{'label':'Apple','value':'AAPL'},
+							{'label':'Google','value':'GOOG'},
+							{'label':'Netflix','value':'NFLX'}
+						],
+						value=['GOOG'],                
+						multi=True)
+		], className="four columns"),
+		# Block 2
+		html.Div([
+			html.Label('Select Start and End dates:'),
+			dcc.DatePickerRange(
+				id='date-range-picker',
+				start_date=dt.today()-timedelta(days=14),
+				end_date=dt.today(),
+				max_date_allowed=dt.today() 
+			)
+		], className="four columns"),
+		# Block 3
+		html.Div([
+			html.Button(id='submit-button', n_clicks=0, children='Submit')
+		
+		], className="four columns"),
+	], className="twelve columns"),
 
-        dcc.Dropdown(id='msize',
-                        options=[{'label':i,'value':i} for i in df['MarketSize'].unique()],
-                        value="Small"
-                        )
-    ], className="twelve columns"),
+	html.Hr(),
 
-    html.Div(id='msize-output'),
-    html.Hr(),
+	html.Div([
+		dcc.Graph(id='my-graph')
+	], className="twelve columns"),
 
-    html.Div([
-       dcc.Dropdown(id='week',
-                        options=[{'label':i,'value':i} for i in df['week'].unique()],
-                        value=1
-                        )
-    ],className="twelve columns"),
-    html.Div(id='week-output'),
+	html.Hr(),
 
-    html.Div(id='display-table')
-
+	html.Div([ 
+		html.Label("In this block we can have extra stuff")
+	], className="twelve columns temp-div")
 
 ], className='twelve columns')
 
-#--------------------------------------------#
+#----------------Callbacks----------------------------#
+@app.callback(Output('my-graph','figure'),
+			[Input('submit-button','n_clicks')],
+			[State('stock-picker','value'),
+			State('date-range-picker','start_date'),
+			State('date-range-picker','end_date')   
+			])
 
-@app.callback(Output('msize-output','children'),
-                [Input('msize','value')])
-def callback_a(msize):
-    return "You have chosen {} market size".format(msize)
-
-@app.callback(Output('week-output','children'),
-                [   Input('week','value')])
-def callback_b(week):
-    return "You have chosen week: {}".format(week)
-
-@app.callback(Output('display-table','children'),
-                [Input('msize','value'),
-                Input('week','value')])
-
-def callback_df(msize,week):
-    query=df[(df['MarketSize']==msize) & (df['week']==week)].groupby(['Promotion']).mean()['SalesInThousands']
-    data=pd.DataFrame(query).reset_index()[['Promotion','SalesInThousands']]
-    columns =  [{"name": i, "id": i,} for i in (data.columns)]
-    return dash_table.DataTable(data=data.to_dict('records'), columns=columns)
+def update_graph(n_clicks,stock_ticker,start,end):
+	data_source='yahoo'
+	traces=[]
+	for i in stock_ticker:
+		df=web.DataReader(i,data_source,start,end).reset_index()
+		df['Stock Name']=i
+		traces.append(df)
+	data=pd.concat(traces)
+	fig = px.line(data, x="Date", y="Close", color='Stock Name', title="Stock Prices")
+	return fig
 
 if __name__=='__main__':
-    app.run_server(debug=True)
+	app.run_server(debug=True, port=8055)
